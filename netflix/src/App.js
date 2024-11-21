c6e1b6f63f4dd63b64cde293d7a7f51203daf13c
meldabolat <meldabolat86@gmail.com>
2024-11-21 15:07:05 +0300

    filtreleme

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
+import { WatchHistoryProvider } from "./context/WatchHistoryContext";
+import { ListProvider } from "./context/ListContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "../src/layout/Layout";
 import Home from "./pages/Home";
-import Favorites from "./pages/Favorites";
+import List from "./pages/List";
 import WatchHistory from "./pages/WatchHistory";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Login ve Register sayfaları */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        
-        {/* Layout'u kapsayan tüm rotalar */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="watchhistory" element={<WatchHistory />} />
-          <Route path="favorites" element={<Favorites />} />
-        </Route>
-      </Routes>
-    </Router>
+    <WatchHistoryProvider>
+      <ListProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="watchhistory" element={<WatchHistory />} />
+              <Route path="list" element={<List />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ListProvider>
+    </WatchHistoryProvider>
   );
 }
 
